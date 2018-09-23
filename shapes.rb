84e38a453fe4c12b8a939dbfd94be31910ac05d7
macadmin <macadmin@MacBook-Pro-de-macadmin.local>
2018-09-23 15:42:26 +0200

    Ajout de gets

--- shapes.rb
+++ shapes.rb
@@ -1,3 +1,14 @@
 # Génération automatique de formes
 puts "Bienvenue dans notre programme de mise en formes !"
 print "Quelle taille voulez-vous pour votre forme ?"
+
+taille_forme = gets
+
+print "Lettre extérieure : "
+lettre_ext = gets
+print "Lettre intérieure : "
+lettre_int = gets
+
+puts "Prêt à dessiner ube forme de taille #{taille_forme}"
+puts "En utilisant #{lettre_ext} pour le contour"
+puts "et #{lettre_int} pour l'intérieur"
